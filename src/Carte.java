419729b417e8933b69992ba3fb3e0d27a5d485bb
Rayanb90 <rayan.benmammar90@gmail.com>
2018-09-12 15:28:36 +0200

    debut classe carte

---
+++ src/Carte.java
@@ -0,0 +1,21 @@
+public class Carte {
+
+    int valeur;
+    String couleur;
+
+    public Carte(String carte){
+
+    }
+
+
+        private String getCouleur() {
+            return this.couleur;
+        }
+
+        private String getValeur() {
+            return this.couleur;
+        }
+
+    }
+
+
