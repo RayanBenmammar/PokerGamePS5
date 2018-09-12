c6b9d11d925cfa3a49b7c31ecec337ce25d33a9c
davidbisegna <david.bisegna@etu.unice.fr>
2018-09-12 22:15:47 +0200

    Ajout enum pour les valeurs des cartes, et une méthode pour comparer deux cartes

--- src/Carte.java
+++ src/Carte.java
@@ -1,22 +1,50 @@
 public class Carte {
 
-    private int valeur;
+    private ValeurCarte valeur;
     private String couleur;
 
-    public Carte(String carte){
-        this.valeur = Integer.parseInt(carte.substring(0,carte.length() - 2));
+    private enum ValeurCarte{
+        None(""), Deux("2"), Trois("3"), Quatre("4"), Cinq("5"), Six("6"), Sept("7"), Huit("8"), Neuf("9"), Dix("10"),
+        Valet("V"), Dame("D"), Roi("R"), As("A");
+
+        private String val;
+
+        ValeurCarte(String s){
+            this.val = s;
+        }
+
+        public String getVal(){
+            return this.val;
+        }
+
+        public static ValeurCarte fromString(String s){
+            for(ValeurCarte val_carte : ValeurCarte.values()){
+                if(s.equals(val_carte.getVal())){
+                    return val_carte;
+                }
+            }
+            return ValeurCarte.None;
+        }
+
+    }
+
+    Carte(String carte){
+        this.valeur = ValeurCarte.fromString(carte.substring(0,carte.length() - 2));
         this.couleur = carte.substring(carte.length() - 2);
     }
 
-
-        public String getCouleur() {
-            return this.couleur;
-        }
-
-        public int getValeur() {
-            return this.valeur;
-        }
-
+    boolean isWeakerThan(Carte c){
+        return this.getValeur().compareTo(c.getValeur()) < 0;
     }
+
+    public String getCouleur() {
+        return this.couleur;
+    }
+
+    ValeurCarte getValeur() {
+        return this.valeur;
+    }
+
+}
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        Carte c1 = new Carte("8Pi");
+        Carte c2 = new Carte("10Pi");
 
-        Carte c1 = new Carte("10Pi");
-
-        System.out.println(c1.getValeur());
-
+        if(c2.isWeakerThan(c1)){
+            System.out.println("La carte gagnante est : " + c1.getValeur());
+        } else {
+            System.out.println("La carte gagnante est : " + c2.getValeur());
+        }
 
     }
 }
