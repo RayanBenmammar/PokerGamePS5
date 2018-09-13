02d0703160f258e5bb2813917714c997ad94e87f
davidbisegna <david.bisegna@etu.unice.fr>
2018-09-13 08:57:39 +0200

    Ajout enum pour les couleurs. Ajout égalité.

--- src/Carte.java
+++ src/Carte.java
@@ -1,7 +1,7 @@
 public class Carte {
 
     private ValeurCarte valeur;
-    private String couleur;
+    private CouleurCarte couleur;
 
     private enum ValeurCarte{
         None(""), Deux("2"), Trois("3"), Quatre("4"), Cinq("5"), Six("6"), Sept("7"), Huit("8"), Neuf("9"), Dix("10"),
@@ -28,16 +28,39 @@ public class Carte {
 
     }
 
+    private enum CouleurCarte{
+        None(""), Pique("Pi"), Trefle("Tr"), Carreau("Ca"), Coeur("Co");
+
+        private String coul;
+
+        CouleurCarte(String s) { this.coul = s;}
+
+        public String getCoul() {return this.coul;}
+
+        public static CouleurCarte fromString(String s){
+            for(CouleurCarte coul_carte : CouleurCarte.values()){
+                if(s.equals(coul_carte.getCoul())){
+                    return coul_carte;
+                }
+            }
+            return CouleurCarte.None;
+        }
+    }
+
     Carte(String carte){
         this.valeur = ValeurCarte.fromString(carte.substring(0,carte.length() - 2));
-        this.couleur = carte.substring(carte.length() - 2);
+        this.couleur = CouleurCarte.fromString(carte.substring(carte.length() - 2));
     }
 
     boolean isWeakerThan(Carte c){
         return this.getValeur().compareTo(c.getValeur()) < 0;
     }
 
-    public String getCouleur() {
+    boolean isSameColor(Carte c) { return this.getCouleur().equals(c.getCouleur());}
+
+    boolean equals(Carte c) { return this.getValeur().equals(c.getValeur()); }
+
+    public CouleurCarte getCouleur() {
         return this.couleur;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        Carte c1 = new Carte("8Pi");
-        Carte c2 = new Carte("10Pi");
+        Carte c1 = new Carte("8Ca");
+        Carte c2 = new Carte("5Pi");
 
         if(c2.isWeakerThan(c1)){
-            System.out.println("La carte gagnante est : " + c1.getValeur());
+            System.out.println("La carte gagnante est : " + c1.getValeur() + " de " + c1.getCouleur());
+        } else if (c2.equals(c1)) {
+            System.out.println("Egalité !");
         } else {
-            System.out.println("La carte gagnante est : " + c2.getValeur());
+            System.out.println("La carte gagnante est : " + c2.getValeur() + " de " + c2.getCouleur());
         }
 
     }
