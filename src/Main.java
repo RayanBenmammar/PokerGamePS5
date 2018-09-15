e46b4f564e7c5eb22dfc716a86aef1e4f00359ea
Rayanb90 <rayan.benmammar90@gmail.com>
2018-09-15 18:44:03 +0200

    systeme de saisi tres simpliste ( V1 )

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Carte c1 = new Carte("8Ca");
-        Carte c2 = new Carte("5Pi");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("veuillez entrer la main1, avec la bonne forme : ");
+        String str = sc.next();
+        System.out.println("vous avez saisi la carte : " + str);
+        Carte c1 = new Carte(str);
+
+        sc.nextLine();
+
+        System.out.println("veuillez entrer la main1, avec la bonne forme : ");
+        String str2 = sc.next();
+        System.out.println("vous avez saisi la carte : " + str2);
+        Carte c2 = new Carte(str2);
+
 
         if(c2.isWeakerThan(c1)){
             System.out.println("La carte gagnante est : " + c1.getValeur() + " de " + c1.getCouleur());
