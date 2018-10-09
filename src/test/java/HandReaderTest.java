0cd46b43a6832f525657aaa6592052653f11bb8e
BastienACHARD <bastien.achard@live.fr>
2018-10-09 08:45:05 +0200

    Update : test de la class HandReader : test sur la method checkWinner

--- src/test/java/HandReaderTest.java
+++ src/test/java/HandReaderTest.java
@@ -18,6 +18,8 @@ class HandReaderTest {
     List<Card> list7;
     List<Card> list8;
     List<Card> list9;
+    List<Card> list10;
+    List<Card> list11;
     Hand hand1;
     Hand hand2;
     Hand hand3;
@@ -27,6 +29,8 @@ class HandReaderTest {
     Hand hand7;
     Hand hand8;
     Hand hand9;
+    Hand hand10;
+    Hand hand11;
 
     @BeforeEach
     public void setup(){
@@ -39,6 +43,8 @@ class HandReaderTest {
         list7= new ArrayList<>(Arrays.asList(new Card("APi"),new Card("ATr"),new Card("ACo"),new Card("2Pi"), new Card("2Tr")));
         list8= new ArrayList<>(Arrays.asList(new Card("9Pi"),new Card("9Co"),new Card("9Ca"),new Card("9Tr"),new Card("5Pi")));
         list9= new ArrayList<>(Arrays.asList(new Card("9Pi"),new Card("8Pi"),new Card("7Pi"),new Card("6Pi"),new Card("5Pi")));
+        list10= new ArrayList<>(Arrays.asList(new Card("2Pi"),new Card("8Co"),new Card("RPi"),new Card("6Pi"),new Card("5Pi")));
+        list11= new ArrayList<>(Arrays.asList(new Card("9Pi"),new Card("9Ca"),new Card("DCo"),new Card("6Pi"),new Card("5Pi")));
 
 
         hand1 = new Hand(list1);
@@ -50,6 +56,8 @@ class HandReaderTest {
         hand7 = new Hand(list7);
         hand8 = new Hand(list8);
         hand9= new Hand(list9);
+        hand10= new Hand(list10);
+        hand11= new Hand(list11);
 
     }
 
@@ -106,6 +114,11 @@ class HandReaderTest {
         assertEquals(0, reader.checkWinner(hand8, hand4));
         assertEquals(0, reader.checkWinner(hand9, hand8));
         assertEquals(0, reader.checkWinner(hand7, hand2));
+
+        assertEquals(-1, reader.checkWinner(hand7, hand7));
+        assertEquals(-1, reader.checkWinner(hand3, hand3));
+        assertEquals(-1, reader.checkWinner(hand5, hand5));
+
     }
 
 }
